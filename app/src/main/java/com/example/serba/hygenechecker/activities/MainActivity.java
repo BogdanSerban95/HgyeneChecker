0e98ce35d14b6b7ac1a21c164a852968d3ee5053
Bogdan <serbanbogdan2008@yahoo.com>
2018-03-01 00:38:59 +0000

    wip advanced search

--- app/src/main/java/com/example/serba/hygenechecker/activities/MainActivity.java
+++ app/src/main/java/com/example/serba/hygenechecker/activities/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.serba.hygenechecker.activities;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.transition.TransitionManager;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.serba.hygenechecker.R;
 import com.example.serba.hygenechecker.models.SearchParams;
@@ -13,17 +23,69 @@ import com.example.serba.hygenechecker.models.SearchParams;
 public class MainActivity extends AppCompatActivity {
     public static final String SEARCH_PARAMS = "search_parameters";
 
-    private TextInputEditText businessNameEditText;
-    private TextInputEditText businessAddressEditText;
-    private TextInputLayout nameInputLayout;
-    private TextInputLayout addressInputLayout;
+    private boolean isAdvancedSearch = false;
+
+    private EditText businessNameEditText;
+    private EditText businessAddressEditText;
+    private TextView radiusTextView;
+    private SeekBar radiusSeekBar;
+    private Button advancedSearchButton;
+    private Button localSearchButton;
+    private View advancedSearchGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final ConstraintSet constraintSet1 = new ConstraintSet();
+        final ConstraintSet constraintSet2 = new ConstraintSet();
+
+        constraintSet2.clone(this, R.layout.activity_mail_adv);
+        constraintSet2.setVisibility(R.id.advanced_search_group, ConstraintSet.VISIBLE);
         setContentView(R.layout.activity_main);
 
+        final ConstraintLayout constraintLayout = findViewById(R.id.main_constraint_layout);
+        constraintSet1.clone(constraintLayout);
+
         initViews();
+
+        radiusSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                double progress = i / 10.0;
+                radiusTextView.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        advancedSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TransitionManager.beginDelayedTransition(constraintLayout);
+                if (!isAdvancedSearch) {
+                    advancedSearchButton.setText(getResources().getString(R.string.simple_search));
+                    constraintSet1.setVisibility(R.id.advanced_search_group, ConstraintSet.VISIBLE);
+//                    constraintSet2.applyTo(constraintLayout);
+//                    advancedSearchGroup.setVisibility(View.VISIBLE);
+//                    localSearchButton.setVisibility(View.GONE);
+                } else {
+                    advancedSearchButton.setText(getResources().getString(R.string.advanced_search));
+                    constraintSet1.setVisibility(R.id.advanced_search_group, ConstraintSet.GONE);
+//                    constraintSet1.applyTo(constraintLayout);
+//                    advancedSearchGroup.setVisibility(View.GONE);
+//                    localSearchButton.setVisibility(View.VISIBLE);
+                }
+                isAdvancedSearch = !isAdvancedSearch;
+            }
+        });
 
         findViewById(R.id.search_button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,12 +116,8 @@ public class MainActivity extends AppCompatActivity {
     private boolean areViewsValid() {
         boolean valid = true;
         if (businessAddressEditText.getText().toString().isEmpty() && businessNameEditText.getText().toString().isEmpty()) {
-            addressInputLayout.setError(getResources().getString(R.string.empty_search_fields_error));
-            nameInputLayout.setError(getResources().getString(R.string.empty_search_fields_error));
             valid = false;
         } else {
-            addressInputLayout.setError(null);
-            nameInputLayout.setError(null);
         }
         return valid;
     }
@@ -67,9 +125,11 @@ public class MainActivity extends AppCompatActivity {
     private void initViews() {
         businessNameEditText = findViewById(R.id.business_name_edit_text);
         businessAddressEditText = findViewById(R.id.business_address_edit_text);
-        nameInputLayout = findViewById(R.id.name_input_layout);
-        addressInputLayout = findViewById(R.id.address_input_layout);
-
+        radiusTextView = findViewById(R.id.radius_label);
+        radiusSeekBar = findViewById(R.id.radius_seek_bar);
+        advancedSearchButton = findViewById(R.id.advanced_search_button);
+        localSearchButton = findViewById(R.id.local_search_button);
+        advancedSearchGroup = findViewById(R.id.advanced_search_group);
     }
 
 }
